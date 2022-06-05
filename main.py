150e6a841cb388a0d7265cc769b06a3f2b36c13e
Wojciech Kabatc <wojciechkabatc@gmail.com>
2022-06-05 16:31:30 +0200

    Wersja v1

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
+import csv, sqlite3
+
+class ReportGenerator:
+  def __init__(self,connection, escape_string = "(%s)"):
+    self.connection = connection
+    self.report_text = None
+    self.escape_string = escape_string
+
+  def generate_report(self):
+    cursor = self.connection.cursor()
+    sql_query = f"Select sum(duration) from polaczenia"
+    cursor.execute(sql_query)
+    result = cursor.fetchone()[0]
+    self.report_text = result
+
+  def get_report(self):	
+    return self.report_text
+
+inputFile = str(input())
+
+sqlite_con = sqlite3.connect("moja_baza.db", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+cur = sqlite_con.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS polaczenia (from_subscriber data_type INTEGER, 
+                  to_subscriber data_type INTEGER, 
+                  datetime data_type timestamp, 
+                  duration data_type INTEGER , 
+                  celltower data_type INTEGER);''')
+
+with open(inputFile,'r') as fin: 
+    reader = csv.reader(fin, delimiter = ";")
+    next(reader, None)
+    rows = [x for x in reader]
+    cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) VALUES (?, ?, ?, ?, ?);", rows)
+    sqlite_con.commit()
 
 if __name__ == "__main__":
+    rg = ReportGenerator(sqlite_con, escape_string="?")
+    rg.generate_report()
+    print(rg.get_report())
     pass
